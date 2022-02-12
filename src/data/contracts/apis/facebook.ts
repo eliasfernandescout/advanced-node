6033182d447e36b25e613ed7b5c005992ead50fc
Elias Fernandes <eliasfernandescout@gmail.com>
2022-02-12 19:19:36 -0300

    chore: add a IloardUserByEmailRepository interface

--- src/data/contracts/apis/facebook.ts
+++ src/data/contracts/apis/facebook.ts
@@ -1,12 +1,19 @@
 export interface ILoadFacebookUserApi {
-    loadUser: (params: LoadFacebookUserApi.Params) => Promise<LoadFacebookUserApi.Result>
+  loadUser: (params: LoadFacebookUserApi.Params) => Promise<LoadFacebookUserApi.Result>
 
 }
 
+export interface ILoadUserByEmailRepository {
+  loadUserByEmail: (email: string) => Promise<void>
+}
 export namespace LoadFacebookUserApi {
-    export type Params = {
-        token: string
-    }
+  export type Params = {
+    token: string
+  }
 
-    export type Result = undefined
+  export type Result = undefined | {
+    facebookId: string
+    email: string
+    name: string
+  }
 }
