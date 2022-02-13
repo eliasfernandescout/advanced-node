e50828924f95f71779e8cc666b31f77ac825e547
Elias Fernandes <eliasfernandescout@gmail.com>
2022-02-13 19:33:11 -0300

    refactor: simplify code using Intersection Types

--- src/data/use-cases/facebook-authentication.ts
+++ src/data/use-cases/facebook-authentication.ts
@@ -17,3 +17,20 @@ export class FacebookAuthentcationUseCase {
     return new AuthenticationError()
   }
 }
+
+// #################################################################################
+// -------> OUTRO METODO DE ESCREVER O CODIGO ACIMA
+
+// export class FacebookAuthentcationUseCase {
+//   constructor(
+//     private readonly facebookApi: ILoadFacebookUserApi,
+//     private readonly userAccountRepository: ILoadUserRepository & ICreateFacebookAccountRepository
+//   ) { }
+
+//   async perform(params: FacebookAuthentication.Params): Promise<AuthenticationError> {
+//     const facebookData = await this.facebookApi.loadUser(params)
+//     await this.userAccountRepository.load({ email: facebookData?.email })
+//     await this.userAccountRepository.createFromFacebook(facebookData)
+//     return new AuthenticationError()
+//   }
+// }
