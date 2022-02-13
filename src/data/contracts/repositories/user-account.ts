321638c25280324783b91e8be55f5e9acf66ba5e
Elias Fernandes <eliasfernandescout@gmail.com>
2022-02-13 19:27:53 -0300

    feat: ensure use-caswe calls ICreateFacebookAccountRepository when ILoadUserRepository returns undefined

--- src/data/contracts/repositories/user-account.ts
+++ src/data/contracts/repositories/user-account.ts
@@ -1,9 +1,23 @@
 export interface ILoadUserRepository {
-  load: (params: NLoadUserByEmailRepository.Params) => Promise<void>
+  load: (params: NLoadUserAccountRepository.Params) => Promise<NLoadUserAccountRepository.Result>
 
 }
-export namespace NLoadUserByEmailRepository {
+export namespace NLoadUserAccountRepository {
   export type Params = {
     email: string
+  }
+
+  export type Result = undefined
+}
+
+export interface ICreateFacebookAccountRepository {
+  createFromFacebook: (params: NCreateFacebookAccountRepository.Params) => Promise<void>
+
+}
+export namespace NCreateFacebookAccountRepository {
+  export type Params = {
+    email: string
+    name: string
+    facebookId: string
   }
 }
--- src/data/use-cases/facebook-authentication.ts
+++ src/data/use-cases/facebook-authentication.ts
@@ -1,17 +1,19 @@
 import { ILoadFacebookUserApi } from '@/data/contracts/apis'
-import { ILoadUserRepository } from '@/data/contracts/repositories'
+import { ILoadUserRepository, ICreateFacebookAccountRepository } from '@/data/contracts/repositories'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/use-cases'
 
 export class FacebookAuthentcationUseCase {
   constructor(
     private readonly loadFacebookUserApi: ILoadFacebookUserApi,
-    private readonly loadUserAccountRepository: ILoadUserRepository
+    private readonly loadUserAccountRepository: ILoadUserRepository,
+    private readonly createFacebookAccountRepository: ICreateFacebookAccountRepository
   ) { }
 
   async perform(params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const facebookData = await this.loadFacebookUserApi.loadUser(params)
     await this.loadUserAccountRepository.load({ email: facebookData?.email })
+    await this.createFacebookAccountRepository.createFromFacebook(facebookData)
     return new AuthenticationError()
   }
 }
