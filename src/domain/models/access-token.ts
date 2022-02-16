d55ed7a8bddc51a52f4c9c5e9c706a5b46b6b003
Elias Fernandes <eliasfernandescout@gmail.com>
2022-02-15 22:24:14 -0300

    feat: create a access-token in model to ensure it expires in 30 minutes

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,3 +1,6 @@
-export type AccessToken = {
-    accesToken: string
+export class AccessToken {
+  constructor(private readonly value: string) { }
+  static get expirationInMs(): number {
+    return 30 * 60 * 1000
+  }
 }
